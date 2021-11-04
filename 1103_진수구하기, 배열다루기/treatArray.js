62f4082d8e4e2de44ca79e4e22a9e34754116306
sju02048 <sju02048@naver.com>
2021-11-04 21:24:46 +0900

    1104_update

--- 1103_진수구하기, 배열다루기/treatArray.js
+++ 1103_진수구하기, 배열다루기/treatArray.js
@@ -16,6 +16,7 @@ function calculate(m) {
   }
   console.log(result);
 }
+// calculate(4);
 
 // 이전 값 곱하기
 // 1  2  6  24  120
@@ -27,7 +28,6 @@ function calculate_2(m) {
   }
   console.log(result);
 }
-// calculate(4);
 // calculate_2(6);
 
 /*
@@ -135,12 +135,129 @@ const data = {
   },
 };
 
-function makeNumberArray(data) {
-  const upper_key = Object.keys(data);
-  for (i in upper_key) {
-    const keys = Object.values(data[upper_key[i]]);
-    console.log(data[upper_key[i]]);
-  }
+function onlyNumberValue(result, arg) {
+  const keys = Object.keys(arg);
+  const values = Object.values(arg);
+  values.forEach((value, i) => {
+    if (typeof value === "object") onlyNumberValue(result, value);
+    else if (typeof value === "number") result.push(keys[i]);
+  });
+  return result;
 }
+// console.log(onlyNumberValue([], data));
 
-makeNumberArray(data);
+/*
+  5. 배열 결과 출력
+  아래 링크를 눌러서 데이터를 확인한다.
+  type이 sk인, name으로 구성된 배열만 출력해본다.
+  https://gist.github.com/crongro/a9a118977f82780441db664d6785efe3
+
+  실행결과
+  ["Yong", "hary", "solvin", "hani", "chulsu"]
+*/
+
+const list = [
+  {
+    id: 1,
+    name: "Yong",
+    phone: "010-0000-0000",
+    type: "sk",
+    childnode: [
+      {
+        id: 11,
+        name: "echo",
+        phone: "010-0000-1111",
+        type: "kt",
+        childnode: [
+          {
+            id: 115,
+            name: "hary",
+            phone: "211-1111-0000",
+            type: "sk",
+            childnode: [
+              {
+                id: 1159,
+                name: "pobi",
+                phone: "010-444-000",
+                type: "kt",
+                childnode: [
+                  {
+                    id: 11592,
+                    name: "cherry",
+                    phone: "111-222-0000",
+                    type: "lg",
+                    childnode: [],
+                  },
+                  {
+                    id: 11595,
+                    name: "solvin",
+                    phone: "010-000-3333",
+                    type: "sk",
+                    childnode: [],
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            id: 116,
+            name: "kim",
+            phone: "444-111-0200",
+            type: "kt",
+            childnode: [
+              {
+                id: 1168,
+                name: "hani",
+                phone: "010-222-0000",
+                type: "sk",
+                childnode: [
+                  {
+                    id: 11689,
+                    name: "ho",
+                    phone: "010-000-0000",
+                    type: "kt",
+                    childnode: [
+                      {
+                        id: 116890,
+                        name: "wonsuk",
+                        phone: "010-000-0000",
+                        type: "kt",
+                        childnode: [],
+                      },
+                      {
+                        id: 1168901,
+                        name: "chulsu",
+                        phone: "010-0000-0000",
+                        type: "sk",
+                        childnode: [],
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            id: 117,
+            name: "hong",
+            phone: "010-0000-0000",
+            type: "lg",
+            childnode: [],
+          },
+        ],
+      },
+    ],
+  },
+];
+
+function findNameInSK(result, arg) {
+  let values = Object.values(arg);
+  values.forEach((value, i) => {
+    if (value.type === "sk") result.push(value.name);
+
+    findNameInSK(result, value.childnode);
+  });
+
+  return result;
+}
+// console.log(findNameInSK([], list));
