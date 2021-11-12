8982e56f07651a13b82e94df852ff29cee40edca
sju02048 <sju02048@naver.com>
2021-11-12 15:42:16 +0900

    급해서 패스 ㅠㅠㅠ

--- 1110_bracketGrammerCheck.js/grammerCheck_stack.js
+++ 1110_bracketGrammerCheck.js/grammerCheck_stack.js
@@ -6,9 +6,11 @@
 
     숫자값이 두자리 이상일 때  
     쉼표가 있을 때는 어떻게 해야할까??
-    flat??? 
-    if(/\[/.test("[")/) -> 배열의 시작 만들기
+    flat??을 활용하고 싶지만 data값을 온전한 array로 만들기 어렵디.
     JSON.stringify(result, null, 2) -> 예쁘게 출력?
+    JSON.parse는 쓰지말자
+
+    11-12: 1,2번은 잘 되지만 3번이 해결이 안되네 하ㅏㅏ 조건 하나하나 맞추기가 너무 헷갈림
  */
 
 // test code
@@ -16,20 +18,20 @@ const data1 = "[1,2,[3,4,[5,[6]]]]"; // 기본
 const data2 = "][1,2,[3,4,[5,[6]]]"; // 괄호가 문제있을 때
 const data3 = "[1,[2,3,[4]]]"; // 한 깊이에 괄호가 여러개 있을 때
 const data4 = "[17,2,[333,[520,[6]]]]"; // 두 자리 이상의 숫자
-const data5 = "[1,[2],[3,4,[5,[6],7],2,5]]"; // 뒷 자리에도 값이 있을 때
+const data5 = "[1,2,[3,4],[[6,[7]],[8],43]]"; // 뒷 자리에도 값이 있을 때
 run(data5);
 
+// 문법 체크
 function run(data) {
-  let stack = []; // 괄호 깊이를 알기위한 스택
+  let stack = []; // 문법검사용
   const onlyNumArr = data.replace(/[\[\]]/g, "").split(","); // 숫자만 남기게
 
-  // stack에 "[" "]"를 넣어 문법 검사
+  // stack에 "[" "]"를 넣어 짝이 맞는지 문법 검사
   for (let i = 0; i < data.length; i++) {
     if (data[i] === "[") stack = [...stack, data[i]];
     else if (data[i] === "]")
       if (!stack.pop()) return console.log(`2. 여는 괄호가 부족합니다.`);
   }
-
   // 스택길이가 아직 남아있으면 닫는 괄호에 문제있음.
   stack.length
     ? console.log(`2. 닫는 괄호가 부족합니다.`)
@@ -40,15 +42,60 @@ function run(data) {
       );
 }
 
+// 깊이 수준 구하기
 function getDepthLevel(data) {
-  const arr = data.split("");
-  const leftBracketLastIndex = arr.lastIndexOf("[");
-  let leftBracketCount = 0;
-  let rightBracketCount = 0;
-  for (let i = 0; i <= leftBracketLastIndex; i++) {
-    if (arr[i] === "[") leftBracketCount++;
-    else if (arr[i] === "]") rightBracketCount++;
+  let depthArr = [];
+  let count = 0;
+  const onlyBracketArr = data.replace(/[^\[\]]/g, "");
+
+  // 현재 depth를 계속 넣어줌
+  for (let val of onlyBracketArr) {
+    if (val === "[") depthArr = [...depthArr, ++count];
+    else if (val === "]") depthArr = [...depthArr, count--];
   }
-  // "["의 개수 - "]"의 개수 = 최대깊이
-  return leftBracketCount - rightBracketCount;
+
+  return Math.max(...depthArr);
 }
+
+// 3. 배열 분석 정보를 출력
+// 첫번째 "["를 찾고 뒤에서부터 ]를 찾는다. splice로 index만큼 나누어서
+// 바깥쪽 값들은 해당 depth만큼의 값, 안쪽 값들은 재귀함수로 다시 돌림.
+
+// 문제있음.. 11-12: 해결못함
+// 두자리 이상의 숫자 ex) 102 같은 것을 하나의 인자로 받아야하는데 그게 잘 안됨.
+// 재귀 쓰는 것도 어렵고 배열 안의 배열까지 생각하려니까 너무 헷갈린다. 하ㅏ하하ㅏㅏㅏ
+function printArrayInfo(data) {
+  let splData = data.split("");
+  console.log(splData);
+  const firstLeftBracket = function (val) {
+    return val.indexOf("[");
+  };
+  const lastRightBracket = function (val) {
+    return val.lastIndexOf("]");
+  };
+  console.log(splData);
+  const newArr = splData.splice(
+    firstLeftBracket(splData) + 1,
+    lastRightBracket(splData) - 1
+  );
+  console.log(splData);
+  console.log(newArr);
+  const newNewArr = newArr.splice(
+    firstLeftBracket(newArr) + 1,
+    lastRightBracket(newArr) - 1
+  );
+  console.log(newArr);
+  console.log(newNewArr);
+
+  /*
+  obj = {};
+  let testobj = {};
+  obj.child = [...obj.child, getChildObj(2)];
+  obj.child = [...obj.child, getChildObj(3)];
+  testobj = obj.child;
+  testobj[0].child = [...testobj[0].child, getChildObj(4)];
+  console.log(obj);
+  */
+}
+// printArrayInfo(data5);
+// [1,2,[3,4],[[6,[7]],[8],43]] depth:4, number:10
--- 1110_bracketGrammerCheck.js/재귀연습.js
+++ 1110_bracketGrammerCheck.js/재귀연습.js
@@ -1,7 +1,9 @@
-// 3으로 시작, 6으로 끝나게
-// 들어갈때마다 +1
-// solution 함수의 리턴값의 타입은 함수가 되어야합니다
-// 클로저연습
+/*
+    클로저연습
+    3으로 시작, 6으로 끝나게
+    들어갈때마다 +1
+    solution 함수의 리턴값의 타입은 함수가 되어야합니다
+*/
 function solution(start, end) {
   return function inner() {
     if (start <= end) return start++;
@@ -9,10 +11,19 @@ function solution(start, end) {
 }
 
 let gfunc = solution(3, 6);
-console.log(gfunc());
-console.log(gfunc());
-console.log(gfunc());
-console.log(gfunc());
-console.log(gfunc());
+// console.log(gfunc());
+// console.log(gfunc());
+// console.log(gfunc());
+// console.log(gfunc());
+// console.log(gfunc());
 
 // 재귀함수를 연습해보자
+
+// 꼬리 재귀
+pow(2, 4); // 16
+function pow(x, n, total = 1) {
+  //x의 n승
+  if (n === 0) return total;
+  return pow(x, n - 1, total * x);
+}
+console.log(pow(2, 4));
