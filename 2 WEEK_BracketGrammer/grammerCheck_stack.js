760308ad0e41ee6a42d77945dafa4676444c00f3
sju02048 <sju02048@naver.com>
2021-11-17 02:43:11 +0900

    🎨 생성 donny의 getDataStructure 함수

--- 2 WEEK_BracketGrammer/grammerCheck_stack.js
+++ 2 WEEK_BracketGrammer/grammerCheck_stack.js
@@ -18,28 +18,26 @@ const data1 = "[1,2,[3,4,[5,[6]]]]"; // 기본
 const data2 = "][1,2,[3,4,[5,[6]]]"; // 괄호가 문제있을 때
 const data3 = "[1,[2,3,[4]]]"; // 한 깊이에 괄호가 여러개 있을 때
 const data4 = "[17,2,[333,[520,[6]]]]"; // 두 자리 이상의 숫자
-const data5 = "[1,2,[3,4],[[6,[7]],[8],43]]"; // 뒷 자리에도 값이 있을 때
+const data5 = "[1,2,[3,4],[[6,[7]],[8],43],99]"; // 뒷 자리에도 값이 있을 때
 run(data5);
 
-// 문법 체크
 function run(data) {
-  let stack = []; // 문법검사용
+  let stack = [];
   const onlyNumArr = data.replace(/[\[\]]/g, "").split(","); // 숫자만 남기게
 
-  // stack에 "[" "]"를 넣어 짝이 맞는지 문법 검사
   for (let i = 0; i < data.length; i++) {
     if (data[i] === "[") stack = [...stack, data[i]];
     else if (data[i] === "]")
       if (!stack.pop()) return console.log(`2. 여는 괄호가 부족합니다.`);
   }
-  // 스택길이가 아직 남아있으면 닫는 괄호에 문제있음.
-  stack.length
-    ? console.log(`2. 닫는 괄호가 부족합니다.`)
-    : console.log(
-        `1. 배열의 중첩된 깊이 수준은 ${getDepthLevel(data)}이며, 총 ${
-          onlyNumArr.length
-        }개의 원소가 포함되어 있습니다.`
-      );
+
+  if (stack.length) console.log(`2. 닫는 괄호가 부족합니다.`);
+  else
+    console.log(
+      `1. 배열의 중첩된 깊이 수준은 ${getDepthLevel(data)}이며, 총 ${
+        onlyNumArr.length
+      }개의 원소가 포함되어 있습니다.`
+    );
 }
 
 // 깊이 수준 구하기
@@ -58,44 +56,51 @@ function getDepthLevel(data) {
 }
 
 // 3. 배열 분석 정보를 출력
-// 첫번째 "["를 찾고 뒤에서부터 ]를 찾는다. splice로 index만큼 나누어서
-// 바깥쪽 값들은 해당 depth만큼의 값, 안쪽 값들은 재귀함수로 다시 돌림.
+// donny 코딩 클론.
+// JSON.stringify 했을 때 전부보면 data5의 8, 43, 99가 같은 선상에 있다..
+//
+const dataStructure = {
+  type: "root",
+  child: [],
+};
+let parent = dataStructure.child;
 
-// 문제있음.. 11-12: 해결못함
-// 두자리 이상의 숫자 ex) 102 같은 것을 하나의 인자로 받아야하는데 그게 잘 안됨.
-// 재귀 쓰는 것도 어렵고 배열 안의 배열까지 생각하려니까 너무 헷갈린다. 하ㅏ하하ㅏㅏㅏ
-function printArrayInfo(data) {
-  let splData = data.split("");
-  console.log(splData);
-  const firstLeftBracket = function (val) {
-    return val.indexOf("[");
-  };
-  const lastRightBracket = function (val) {
-    return val.lastIndexOf("]");
-  };
-  console.log(splData);
-  const newArr = splData.splice(
-    firstLeftBracket(splData) + 1,
-    lastRightBracket(splData) - 1
-  );
-  console.log(splData);
-  console.log(newArr);
-  const newNewArr = newArr.splice(
-    firstLeftBracket(newArr) + 1,
-    lastRightBracket(newArr) - 1
-  );
-  console.log(newArr);
-  console.log(newNewArr);
+const pushChild = (dataStr) => {
+  let child;
+  if (dataStr === "[") {
+    child = {
+      type: "array",
+      child: [],
+    };
+    parent.push(child);
+    return parent[parent.length - 1].child;
+  } else {
+    child = {
+      type: isNaN(dataStr) ? "string" : "number",
+      value: dataStr,
+      child: [],
+    };
+    parent.push(child);
+  }
+};
 
-  /*
-  obj = {};
-  let testobj = {};
-  obj.child = [...obj.child, getChildObj(2)];
-  obj.child = [...obj.child, getChildObj(3)];
-  testobj = obj.child;
-  testobj[0].child = [...testobj[0].child, getChildObj(4)];
-  console.log(obj);
-  */
-}
+const getDataStructure = (data) => {
+  const splitData = data.split(/[\],]/);
+  console.log(splitData);
+
+  for (let i = 0, len = splitData.length; i < len; i++) {
+    const el = splitData[i];
+    if (el === "") continue;
+    if (el[0] === "[") {
+      parent = pushChild(el[0]);
+      splitData[i] = el.substring(1, el.length);
+    }
+    pushChild(splitData[i]);
+  }
+
+  return dataStructure;
+};
+console.log(JSON.stringify(getDataStructure(data5)));
+
 // printArrayInfo(data5);
 // [1,2,[3,4],[[6,[7]],[8],43]] depth:4, number:10
