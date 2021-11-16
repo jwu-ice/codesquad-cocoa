6e82c8fd82acbbe0cd6176b3cf735f8d8f1a7081
sju02048 <sju02048@naver.com>
2021-11-16 10:53:32 +0900

    확인

--- 1115_Async_GradeManageProgram/SubjectGradeReadFile.js
+++ 1115_Async_GradeManageProgram/SubjectGradeReadFile.js
@@ -17,52 +17,30 @@ fs.readFile(
 const getSubject = (text) => {
   const subjects = JSON.parse(text);
   console.log("과목별 점수:", subjects);
-
-  console.log(subjects);
   console.log("quickSort:", quickSort(subjects));
-};
-
-const quickSort = function (arr) {
-  if (arr.length <= 1) return arr;
-
-  const pivot = arr[0];
-  const left = [];
-  const right = [];
-
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] <= pivot) left.push(arr[i]);
-    else right.push(arr[i]);
-  }
-
-  const lSorted = quickSort(left);
-  const rSorted = quickSort(right);
-  return [...lSorted, pivot, ...rSorted];
 };
 
 // 객체로 풀다 실패........................
 // 객체는 쓰레기다 Map을 쓰자
 
-// const quickSort = function (arr) {
-//   console.log("arr:", arr);
-//   if (Object.keys(arr).length <= 1) return arr;
+const quickSort = function (arr) {
+  if (Object.keys(arr).length <= 1) return arr;
 
-//   const pivot = arr[Object.keys(arr)[0]];
-//   let left = {};
-//   let right = {};
+  const pivot = arr[Object.keys(arr)[0]];
+  let left = {};
+  let right = {};
 
-//   for (let i = 1; i < Object.keys(arr).length; i++) {
-//     let obj = {};
-//     obj[Object.keys(arr)[i]] = arr[Object.keys(arr)[i]];
-//     console.log("obj:", obj);
-//     if (arr[Object.keys(arr)[i]] <= pivot) left = Object.assign(left, obj);
-//     else right = Object.assign(right, obj);
-//   }
-//   const lSorted = quickSort(left);
-//   const rSorted = quickSort(right);
-//   console.log("lSorted :>> ", lSorted);
-//   console.log("rSorted :>> ", rSorted);
-//   console.log("pivot :>> ", pivot);
-//   const pivot_obj = {};
-//   pivot_obj[Object.keys(arr)[0]] = pivot;
-//   return { ...lSorted, ...pivot_obj, ...rSorted };
-// };
+  for (let i = 1; i < Object.keys(arr).length; i++) {
+    let obj = {};
+    obj[Object.keys(arr)[i]] = arr[Object.keys(arr)[i]];
+    if (arr[Object.keys(arr)[i]] <= pivot) Object.assign(left, obj);
+    else Object.assign(right, obj);
+  }
+
+  const lSorted = quickSort(left);
+  const rSorted = quickSort(right);
+  const pivot_obj = {};
+  pivot_obj[Object.keys(arr)[0]] = pivot;
+
+  return { ...lSorted, ...pivot_obj, ...rSorted };
+};
