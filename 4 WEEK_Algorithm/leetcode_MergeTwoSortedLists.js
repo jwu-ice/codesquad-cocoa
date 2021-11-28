02f98175e1810e59a78781176e2bf02a20cb60c6
jwu <sju02048@naver.com>
2021-11-28 21:11:00 +0900

    ✨ 추가 mergeTwoLists while문 사용

--- 4 WEEK_Algorithm/leetcode_MergeTwoSortedLists.js
+++ 4 WEEK_Algorithm/leetcode_MergeTwoSortedLists.js
@@ -21,7 +21,37 @@ var mergeTwoLists = function (list1, list2) {
     return list2;
   }
 };
-// console.log(mergeTwoLists([1, 2, 4], [1, 3, 4]));
+console.log(mergeTwoLists([1, 2, 4], [1, 3, 4]));
+/**
+ * 완벽히 이해하지 못했다..
+ * Linked List 즉, 본인의 값과 next 값을 기억하는 배열인데
+ * 전체배열을 돌기엔 느리고 메모리를 크게 사용하지만
+ * 중간에 값을 넣거나 삭제시 용이하다
+ */
 
-// const node = new ListNode(1, 2, 4);
-// console.log(node);
+// while문과 head 노드 이용
+/*
+const mergeTwoLists = function(l1, l2) {
+  const sentinel = {
+      val: -1,
+      next: null
+  };
+
+  let head = sentinel;
+  while (l1 && l2) {
+      if (l1.val > l2.val) {
+          head.next = l2;
+          l2 = l2.next;
+      } else {
+          head.next = l1;
+          l1 = l1.next;
+      }
+      
+      head = head.next;
+  }
+
+  head.next = l1 || l2;
+
+  return sentinel.next;
+} 
+*/
